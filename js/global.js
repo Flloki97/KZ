2f38cb54f36dae7303dc3328ccdecbfa7b58e64d
flloki97 <you@example.com>
2023-10-04 13:30:31 +0200

    update

--- js/global.js
+++ js/global.js
@@ -1,10 +1,28 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-  var name = document.getElementById("name").value;
-  var email = document.getElementById("email").value;
-  var message = document.getElementById("message").value;
+function sendMail(){
+  var params = {
+      name:document.getElementById("name").value ,
+      lastname:document.getElementById("lastname").value ,
+      email:document.getElementById("email").value ,
+      message:document.getElementById("message").value,
+  };
 
-  if (!name || !email || !message) {
-      alert("Please fill in all required fields.");
-      event.preventDefault();
-  }
-});
+  const serviceID = "service_8s68ocw";
+  const templateID = "template_uk6xyyl"
+
+  emailjs
+      .send(serviceID,templateID,params)
+      .then((res) => {
+          document.getElementById("name").value = "";
+          document.getElementById("lastname").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          document.getElementById("formcontact").style.display = "none";
+          document.getElementById("successmessage").style.display = "block";
+          // alert("your message sent successfully");
+      })
+      .catch((err) => console.log(err));
+  
+
+}
+
